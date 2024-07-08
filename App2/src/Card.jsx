ef81b81d0e7de260d5d5118663bcea1162e6dccd
eizadhamdan <115660876+eizadhamdan@users.noreply.github.com>
2024-07-08 14:48:17 +0530

    changed the file structure

---
+++ App2/src/Card.jsx
@@ -0,0 +1,12 @@
+
+function Card() {
+    return(
+        <div className="card">
+            <img></img>
+            <h2>Eizad Hamdan</h2>
+            <p>A Computer Engineering undergraduate.</p>
+        </div>
+    );
+}
+
+export default Card
