ddccd1d7bd1e78b1a423e0832717310e1b3109b6
eizadhamdan <115660876+eizadhamdan@users.noreply.github.com>
2024-07-11 16:47:58 +0530

    React hook

---
+++ App8/src/App.jsx
@@ -0,0 +1,18 @@
+// React hook- Special function that allows functional components to use React features without
+// writing class components (useState, useEffect, useContext, useReducer, useCallback, etc....)
+
+// useState()- A react hook that allows the creation of a stateful variable 
+// and a setter function to update its value in a Virtual DOM. [name, setName]
+
+
+
+// import MyComponent from "./MyComponent.jsx";
+import Counter from "./Counter.jsx";
+
+function App() {
+
+	// return(<MyComponent />);
+	return(<Counter />);
+}
+
+export default App
