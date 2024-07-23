d04d988597d64b28fcaba6148b7123af70bdcfb5
eizadhamdan <115660876+eizadhamdan@users.noreply.github.com>
2024-07-23 13:57:37 +0530

    useContext hook in react

---
+++ App16/src/App.jsx
@@ -0,0 +1,12 @@
+//useContext() - React hook that allows you to share values between mltiple levels 
+//of components without passing props through each level
+
+import React from "react";
+import ComponentA from "./ComponentA.jsx";
+
+function App() {
+
+  return (<ComponentA />);
+}
+
+export default App
