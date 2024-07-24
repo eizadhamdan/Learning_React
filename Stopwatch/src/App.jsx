871ead094d54a3ed6b8126e1b292fae5f6e90363
eizadhamdan <115660876+eizadhamdan@users.noreply.github.com>
2024-07-24 16:58:38 +0530

    stopwatch app

---
+++ Stopwatch/src/App.jsx
@@ -0,0 +1,14 @@
+import Stopwatch from "./Stopwatch.jsx";
+
+function App() {
+
+  return (
+    <>
+      <Stopwatch></Stopwatch>
+      <br></br>
+      <Stopwatch></Stopwatch>
+    </>
+  );
+}
+
+export default App
