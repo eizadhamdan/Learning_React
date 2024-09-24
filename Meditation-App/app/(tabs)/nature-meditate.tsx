dd9e1dae35b33d8574452352adb274170521b673
eizadhamdan <115660876+eizadhamdan@users.noreply.github.com>
2024-09-24 12:41:25 +0530

    first working version of app

---
+++ Meditation-App/app/(tabs)/nature-meditate.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const NatureMeditate = () => {
+    return (
+        <View className="flex-1 justify-center items-center">
+            <Text>Nature Meditate</Text>
+        </View>
+    )
+}
+
+export default NatureMeditate;
---
+++ Meditation-App/app/index.tsx
@@ -0,0 +1,49 @@
+import { View, Text, ImageBackground, SafeAreaView } from "react-native";
+import React from "react";
+import { LinearGradient } from "expo-linear-gradient";
+import { StatusBar } from "expo-status-bar";
+
+import beachImage from "@/assets/meditation-images/beach.webp";
+import CustomButton from "@/components/CustomButton";
+import { useRouter } from "expo-router";
+
+const App = () => {
+    const router = useRouter();
+
+    return (
+        <View className="flex-1">
+            <ImageBackground 
+                source={beachImage}
+                resizeMode="cover" 
+                className="flex-1"
+            >
+                <LinearGradient 
+                    className="flex-1" 
+                    colors={["rgba(0, 0, 0, 0.4)", "rgba(0, 0, 0, 0.8)"]}
+                >
+                    <SafeAreaView className="flex-1 mx-5 my-8 justify-between">
+                        <View>
+                            <Text className="text-center text-white font-bold text-4xl mt-10">
+                                Meditation App
+                            </Text>
+                            <Text className="text-center text-white text-regular text-2xl mt-3">
+                                Simplifying meditation for everyone
+                            </Text>
+                        </View>
+
+                        <View>
+                            <CustomButton 
+                                onPress={() => router.push("/nature-meditate")}
+                                title="Get Started"
+                            />
+                        </View>
+
+                        <StatusBar style="light" />
+                    </SafeAreaView>
+                </LinearGradient>
+            </ImageBackground>       
+        </View>
+    )
+}
+
+export default App
