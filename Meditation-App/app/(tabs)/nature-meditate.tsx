5c0dbda5f54fd8b49128591e9fa04567d237c05f
eizadhamdan <115660876+eizadhamdan@users.noreply.github.com>
2024-10-04 22:42:50 +0530

    affirmations tab

--- Meditation-App/app/(tabs)/nature-meditate.tsx
+++ Meditation-App/app/(tabs)/nature-meditate.tsx
@@ -1,12 +1,93 @@
-import { View, Text } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { StatusBar } from "expo-status-bar";
+import { router } from "expo-router";
 import React from "react";
+import {
+  FlatList,
+  ImageBackground,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
-const NatureMeditate = () => {
-    return (
-        <View className="flex-1 justify-center items-center">
-            <Text>Nature Meditate</Text>
+import MEDITATION_IMAGES from "@/constants/meditation-images";
+
+import { MEDITATION_DATA, MeditationType } from "@/constants/MeditationData";
+import AppGradient from "@/components/AppGradient";
+
+const Page = () => {
+  return (
+    <View className="flex-1">
+      <AppGradient
+        // Background Linear Gradient
+        colors={["#161b2e", "#0a4d4a", "#766e67"]}
+      >
+        <View className="mb-6">
+          <Text className="text-gray-200 mb-3 font-bold text-4xl text-left">
+            Welcome User
+          </Text>
+          <Text className="text-indigo-100 text-xl font-medium">
+            Start your meditation practice
+          </Text>
         </View>
-    )
-}
+        <View>
+          <FlatList
+            data={MEDITATION_DATA}
+            contentContainerStyle={styles.list}
+            keyExtractor={(item) => item.id.toString()}
+            showsVerticalScrollIndicator={false}
+            renderItem={({ item }) => (
+              <Pressable
+                onPress={() => router.push(`/meditate/${item.id}`)}
+                className="h-48 my-3 rounded-md overflow-hidden"
+              >
+                <ImageBackground
+                  source={MEDITATION_IMAGES[item.id - 1]}
+                  resizeMode="cover"
+                  style={styles.backgroundImage}
+                >
+                  <LinearGradient
+                    // Gradient from transparent to black
+                    colors={["transparent", "rgba(0,0,0,0.8)"]}
+                    style={styles.gradient}
+                  >
+                    <Text className="text-gray-100 text-3xl font-bold text-center">
+                      {item.title}
+                    </Text>
+                  </LinearGradient>
+                </ImageBackground>
+              </Pressable>
+            )}
+          />
+        </View>
+      </AppGradient>
+      <StatusBar style="light" />
+    </View>
+  );
+};
 
-export default NatureMeditate;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  background: {
+    flex: 1,
+  },
+  backgroundImage: {
+    flex: 1,
+    borderRadius: 10,
+    justifyContent: "center",
+  },
+  gradient: {
+    alignItems: "center",
+    height: "100%",
+    justifyContent: "center",
+    width: "100%",
+  },
+  list: {
+    paddingBottom: 150,
+  },
+});
+
+export default Page;
--- Meditation-App/app/index.tsx
+++ Meditation-App/app/index.tsx
@@ -1,49 +1,68 @@
-import { View, Text, ImageBackground, SafeAreaView } from "react-native";
+import { View, Text, Image, ImageBackground } from "react-native";
 import React from "react";
-import { LinearGradient } from "expo-linear-gradient";
+import { SafeAreaView } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
+import CustomButton from "@/components/CustomButton";
+import AppGradient from "@/components/AppGradient";
+import { useRouter } from "expo-router";
+import Animated, {
+  FadeInDown,
+  FadeInUp,
+  withSpring,
+} from "react-native-reanimated";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 import beachImage from "@/assets/meditation-images/beach.webp";
-import CustomButton from "@/components/CustomButton";
-import { useRouter } from "expo-router";
 
 const App = () => {
-    const router = useRouter();
+  const router = useRouter();
 
-    return (
-        <View className="flex-1">
-            <ImageBackground 
-                source={beachImage}
-                resizeMode="cover" 
-                className="flex-1"
-            >
-                <LinearGradient 
-                    className="flex-1" 
-                    colors={["rgba(0, 0, 0, 0.4)", "rgba(0, 0, 0, 0.8)"]}
-                >
-                    <SafeAreaView className="flex-1 mx-5 my-8 justify-between">
-                        <View>
-                            <Text className="text-center text-white font-bold text-4xl mt-10">
-                                Meditation App
-                            </Text>
-                            <Text className="text-center text-white text-regular text-2xl mt-3">
-                                Simplifying meditation for everyone
-                            </Text>
-                        </View>
+  return (
+    <GestureHandlerRootView>
+      <View className="flex-1">
+        <ImageBackground
+          source={beachImage}
+          resizeMode="cover"
+          className="flex-1"
+        >
+          <AppGradient
+            // Background Linear Gradient
+            colors={["rgba(0,0,0,0.4)", "rgba(0,0,0,0.8)"]}
+          >
+            <SafeAreaView className="flex flex-1 px-1 justify-between">
+              <Animated.View
+                entering={FadeInDown.delay(300)
+                  .mass(0.5)
+                  .stiffness(80)
+                  .springify(20)}
+              >
+                <Text className="text-center text-white font-bold text-4xl">
+                  Meditation App
+                </Text>
+                <Text className="text-center text-white font-regular text-2xl mt-3">
+                  Simplifying meditation for everyone
+                </Text>
+              </Animated.View>
 
-                        <View>
-                            <CustomButton 
-                                onPress={() => router.push("/nature-meditate")}
-                                title="Get Started"
-                            />
-                        </View>
+              <Animated.View
+                entering={FadeInDown.delay(300)
+                  .mass(0.5)
+                  .stiffness(80)
+                  .springify(20)}
+              >
+                <CustomButton
+                  onPress={() => router.push("/nature-meditate")}
+                  title="Get Started"
+                />
+              </Animated.View>
 
-                        <StatusBar style="light" />
-                    </SafeAreaView>
-                </LinearGradient>
-            </ImageBackground>       
-        </View>
-    )
-}
+              <StatusBar style="light" />
+            </SafeAreaView>
+          </AppGradient>
+        </ImageBackground>
+      </View>
+    </GestureHandlerRootView>
+  );
+};
 
-export default App
+export default App;
